516d7b04f6b95bc11af0cc5a3f0386a2b994416a
Reza Rizki <rezarizki77@gmail.com>
2023-01-09 15:44:32 +0700

    .

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# Create Localhost to host website
+st.set_page_config(page_title='UHH BPS')
+st.header('Ini adalah Header')
+st.subheader('Ini adalah Subheader')
+
+excel_file = 'UHH.xlsx'
+sheet_name = 'DATA'
+
+df = pd.read_excel(excel_file, sheet_name = sheet_name, header = 1, usecols='A:D')
+st.dataframe(df)
+
+pie_chart = px.pie(df, title='UHH', values='2022', names='Kabupaten/Kota')
+st.plotly_chart(pie_chart)
