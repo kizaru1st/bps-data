4558ee8d5d7ef781de1fcfd7e5fa0ba625032d82
Reza Rizki <rezarizki77@gmail.com>
2023-01-09 18:48:36 +0700

    .

--- app.py
+++ app.py
@@ -12,5 +12,11 @@ sheet_name = 'DATA'
 df = pd.read_excel(excel_file, sheet_name = sheet_name, header = 1, usecols='A:D')
 st.dataframe(df)
 
-pie_chart = px.pie(df, title='UHH', values='2022', names='Kabupaten/Kota')
+pie_chart = px.pie(df, title='UHH pada tahun 2020', values='2020', names='Kabupaten/Kota')
+st.plotly_chart(pie_chart)
+
+pie_chart = px.pie(df, title='UHH pada tahun 2021', values='2021', names='Kabupaten/Kota')
+st.plotly_chart(pie_chart)
+
+pie_chart = px.pie(df, title='UHH pada tahun 2022', values='2022', names='Kabupaten/Kota')
 st.plotly_chart(pie_chart)
